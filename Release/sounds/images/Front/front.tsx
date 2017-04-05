36d24e1e82693ea763216a92d2cb23e1f62584fd
Alberto Lopez Sanchez <albertoxp1@gmail.com>
2017-04-05 16:08:47 +0200

    Hecha la configuracion release

---
+++ Release/sounds/images/Front/front.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Front" tilewidth="32" tileheight="64" tilecount="2" columns="0">
+ <tile id="1">
+  <image width="32" height="64" source="SimpleColumn.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="63" source="ComplexColumn.PNG"/>
+ </tile>
+</tileset>
